7c394c74bf4a18def70f5c579b1b84cf0b84d899
xx.alfix <126504278+jjfabalfie@users.noreply.github.com>
2024-05-15 09:50:33 +0100

    Update process-signup.php

--- process-signup.php
+++ process-signup.php
@@ -1,3 +1,10 @@
 <?php
+if (empty($_POST["name"])) {
+    die("Name is required");
+}
+
+filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) { 
+    die("Valid email is required");
+}
 print_r($_POST);
 ?>
